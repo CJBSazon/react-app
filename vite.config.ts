6e606e7089774ceeb34310284a2dd10f3b5ad6ac
CJBSazon <csazon9@.com>
2024-07-28 15:10:31 +0800

    Fix paths for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-mabsi-soy/', // Ensure this matches your GitHub repository name
   plugins: [react()],
 });
