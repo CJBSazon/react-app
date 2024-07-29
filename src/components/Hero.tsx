4f5ea9bd12a95d2897fc3fda47c48af8162efc6e
CJBSazon <csazon9@.com>
2024-07-30 00:48:23 +0800

    Fix paths for GitHub Pages deployment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ function Hero () {
   return (
       <div>
           {/* Hero Section */}
-          <div className="flex justify-center items-center h-screen bg-cover bg-center" style={{ backgroundImage: "url('/src/assets/img/bg home-d564d4b1.jpg')" }}>
+          <div className="flex justify-center items-center h-screen bg-cover bg-center" style={{ backgroundImage: "url('/src/assets/img/bg home.jpg')" }}>
           <div className="text-center">
               <h1 className="text-5xl text-red-500 font-bold mt-10">MABSI SOY</h1>
               <p className="text-3xl text-red-600 mt-4">PEKA MANYAMAN SILOG KENI SASMUAN</p>
@@ -20,10 +20,10 @@ function Hero () {
           <h2 className="text-4xl text-red-500 text-center">MENU</h2>
           <div className="flex justify-center space-x-8 mt-8">
               <div className="bg-white shadow-lg rounded-lg ">
-                  <img src="/src/assets/img/menu_item1-2fb13c58.jpg" alt="Overload Lugaw" className="w-80 h-96"/>
+                  <img src="/src/assets/img/menu_item1.jpg" alt="Overload Lugaw" className="w-80 h-96"/>
               </div>
               <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-                  <img src="/src/assets/img/menu_item-d801e64b.jpg" alt="Combo Sulit Sarap Meals" className="w-80 h-96"/>
+                  <img src="/src/assets/img/menu_item.jpg" alt="Combo Sulit Sarap Meals" className="w-80 h-96"/>
               </div>
           </div>
           <div className="text-center mt-8">
